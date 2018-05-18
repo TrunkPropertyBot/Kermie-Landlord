d1f8b86e4a6cb7085e5a5c8edefee946e3d16884
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-05-18 12:03:30 +1000

    we aren't exporting so keeping with convention and not assigning a variable to the function

--- src/dialogEngine.js
+++ src/dialogEngine.js
@@ -1,4 +1,43 @@
-const processIntents = (payload, data) => {
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Assistant service
+ * @param  {Object} response The response from the Assistant service
+ * @return {Object}          The response with the updated message
+ */
+const updateMessage = (input, response) => {
+  var responseText = null;
+
+  var msg = processIntents(input, response);
+  if(msg != null){
+    response.output.text = msg;
+    return response;
+  }  
+
+  if (!response.output) {
+    response.output = {};
+  } else {
+    return response;
+  }
+  if (response.intents && response.intents[0]) {
+    var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
+    }
+  }
+  response.output.text = responseText;
+  return response;
+}
+
+function processIntents (payload, data) {
   var intent = getHighestConfidenceIntent(data['intents']);
   var message = null;
 
@@ -47,45 +86,6 @@ function getHighestConfidenceIntent(intentList) {
     }
   }
   return intent;
-}
-
-/**
- * Updates the response text using the intent confidence
- * @param  {Object} input The request to the Assistant service
- * @param  {Object} response The response from the Assistant service
- * @return {Object}          The response with the updated message
- */
-const updateMessage = (input, response) => {
-  var responseText = null;
-
-  var msg = processIntents(input, response);
-  if(msg != null){
-    response.output.text = msg;
-    return response;
-  }  
-
-  if (!response.output) {
-    response.output = {};
-  } else {
-    return response;
-  }
-  if (response.intents && response.intents[0]) {
-    var intent = response.intents[0];
-    // Depending on the confidence of the response the app can return different messages.
-    // The confidence will vary depending on how well the system is trained. The service will always try to assign
-    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intent . In these cases it is usually best to return a disambiguation message
-    // ('I did not understand your intent, please rephrase your question', etc..)
-    if (intent.confidence >= 0.75) {
-      responseText = 'I understood your intent was ' + intent.intent;
-    } else if (intent.confidence >= 0.5) {
-      responseText = 'I think your intent was ' + intent.intent;
-    } else {
-      responseText = 'I did not understand your intent';
-    }
-  }
-  response.output.text = responseText;
-  return response;
 }
 
 module.exports = {
