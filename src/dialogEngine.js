77b91659bdb296b5461d263aa4266698ef6c0bf2
d821128 <samantha.raines@team.telstra.com>
2018-05-28 18:40:13 +1000

    added changes in dialogengine for displaying property info

--- src/dialogEngine.js
+++ src/dialogEngine.js
@@ -1,7 +1,10 @@
 const priceFinder = require('./priceFinder');
+const rentEstimator = require('./predict');
 
 const updateMessage = async (input, response) => {
   var responseText = null;
+  var Z_currentPropertyF;
+  var Z_cuurentAddress;
 
   var msg = await processIntents(input, response);
   if(msg != null){
@@ -40,7 +43,7 @@ const processIntents = async (payload, data) => {
     conversationNode = data.output.nodes_visited[0];
   }
   var message = null;
-
+  console.log(conversationNode);
   // Switch over the various cases for handled intents
   switch(intent){
     case 'imagetest':
@@ -52,22 +55,75 @@ const processIntents = async (payload, data) => {
   }
 
   switch(conversationNode){
+    //dialog node:Look for property
     case 'node_2_1526965411734':
       let property = await priceFinder.suggestProperty(data.input.text);
       if( property.matches.length > 0) {
-        message = 'We found the property';
+      var matchId = await property.matches[0].property.id;
+      // let propertyDetail = await priceFinder
+      Z_currentAddress = await property.matches[0].display;
+      let mainImage = await priceFinder.getPropertyImage(matchId);
+      Z_currentPropertyF = await priceFinder.getPropertyFeature(matchId);
+      console.log(Z_currentPropertyF);
+
+      console.log('image '+ mainImage);
+      var defaltAppend = `?access_token=${process.env.PRICEFINDER_TOKEN}&height=400&width=400`;
+      var imageFullUrl = mainImage+defaltAppend;
+      // https://api.pricefinder.com.au/v1/images/220876438?access_token=17d97e9916dc788d8c9c29f78a793f7
+      message ='<img src='+imageFullUrl+' alt='+Z_currentAddress+'>' + Z_currentAddress;
       }
       else{
         message = 'No property found for this address';
       }
       break;
+
+    case 'node_1_1527404413999':
+      switch(intent){
+        case 'bedrooms':
+        message = 'The property has '+Z_currentPropertyF.bedrooms+' bedrooms.';
+        break;
+        case 'bathrooms':
+        message = 'The property has '+Z_currentPropertyF.bathrooms+' bathrooms.';
+        break;
+        case 'carparking':
+        message = 'The property has '+Z_currentPropertyF.carParks+' carparks.';
+        break;
+      }
+      break;
+
+    // make appointment
+    case 'node_5_1527429752421':
+    message = 'Your appointment for '+ Z_currentAddress +' has been made!';
+    break;
+    
+    case 'node_14_1527147231149':
+    let house = await priceFinder.suggestProperty(data.input.text);
+      if( house.matches.length > 0) {
+        data.context.currentContext = 'addressFound';
+        console.log(data.context.currentContext);
+      }
+      else{
+        data.context.currentContext = 'addressNotFound';
+      }
+      break;
+
+    case 'slot_28_1526280891346':
+    var n_bedroom = 3;
+    var n_bathroom =2;
+    var n_carspace = 1;
+    var n_postcode = 3000;
+    var n_suburb = data.context.suburb;
+    let price = await rentEstimator.predictRent(n_bedroom,n_bathroom,n_carspace,n_suburb,n_postcode);
+    console.log('price: ')+price;
+    break;
   }
 
   return message;
 }
 
 function getImageTestMsg() {
-  return '<img src="https://vignette.wikia.nocookie.net/hmwikia/images/1/11/Cow_tott.jpg/revision/latest?cb=20160114034237" alt="Mountain View">';
+  var imageUrl = "https://vignette.wikia.nocookie.net/hmwikia/images/1/11/Cow_tott.jpg/revision/latest?cb=20160114034237";
+  return '<img src='+imageUrl+' alt="Mountain View">';
 }
 
 function getVideoTestMsg() {
